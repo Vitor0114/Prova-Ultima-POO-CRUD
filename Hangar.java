1139ffd36eeab4d41223a9ddebc4e5fab7eae6ab
Vitor <vitorgustav0114@gmail.com>
2022-11-23 21:16:44 -0300

    PrimeiroCommit

---
+++ Hangar.java
@@ -0,0 +1,14 @@
+public class Hangar {
+    
+    private int id;
+    private String local;
+    private int idAviao;
+
+    public Hangar(int id, String local, int idAviao) {
+        
+        this.id = id;
+        this.local = local;
+        this.idAviao = idAviao;
+    }
+
+}
